3ad2a785801ba3d7ef414ebcd67e319f370a75fc
August Huang <h888866j@126.com>
2023-09-23 20:54:29 +0800

    path change for huangjie's WSL env

--- cv1811h/mkitb_cv1811h.sh
+++ cv1811h/mkitb_cv1811h.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 mkimage -f arceos-cv1811h.its arceos-cv1811h.itb 
-sudo mv  arceos-cv1811h.itb /srv/tftp/
+# sudo mv  arceos-cv1811h.itb /srv/tftp/
+
+sudo mv  arceos-cv1811h.itb /mnt/c/Users/August/Downloads/tftpd64.464/
--- scripts/make/cv1811.mk
+++ scripts/make/cv1811.mk
@@ -1,5 +1,6 @@
 cv1811: build
 	cp $(OUT_BIN) cv1811h/arceos-cv1811.bin
 	mkimage -f cv1811h/arceos-cv1811h.its cv1811h/arceos-cv1811.itb
-	@cp cv1811h/arceos-cv1811.itb /srv/tftp/
+	# @cp cv1811h/arceos-cv1811.itb /srv/tftp/
+	@cp cv1811h/arceos-cv1811.itb /mnt/c/Users/August/Downloads/tftpd64.464/
 	@echo 'Built the FIT-uImage cv1811h/arceos-cv1811.itb'
