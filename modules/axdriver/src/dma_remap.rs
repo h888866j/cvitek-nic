f4352f63d1fd816443269ff5be0b8fda2588dc4b
ZhiyuanSue <2262387848@qq.com>
2023-11-01 18:29:23 +0800

    update

--- modules/axdriver/src/dma_remap.rs
+++ modules/axdriver/src/dma_remap.rs
@@ -2,43 +2,43 @@ use core::ptr::{read_volatile, write_volatile};
 const  REG_SDMA_DMA_CH_REMAP0:usize=	0x03000154;
 const  REG_SDMA_DMA_CH_REMAP1:usize=	0x03000158;
 
-const table_size:usize=8;
+const TABLE_SIZE:usize=8;
 
-struct Dma_Remap_Item{
+struct DmaRemapItem{
     hs_id:usize,
     channel_id:u8,
 }
 
-const REMAP_TABLE:[Dma_Remap_Item;table_size]=[
-    Dma_Remap_Item{
+const REMAP_TABLE:[DmaRemapItem;TABLE_SIZE]=[
+    DmaRemapItem{
         hs_id:13,
         channel_id:1
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:12,
         channel_id:0
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:7,
         channel_id:2
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:0,
         channel_id:3
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:20,
         channel_id:4
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:21,
         channel_id:5
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:2,
         channel_id:6
     },
-    Dma_Remap_Item{
+    DmaRemapItem{
         hs_id:38,
         channel_id:7
     },
@@ -48,7 +48,7 @@ pub fn dma_hs_remap_init()
 {
     let mut remap0_val=0;
     let mut remap1_val=0;
-    for i in 0..table_size{
+    for i in 0..TABLE_SIZE{
         let hs_id=REMAP_TABLE[i].hs_id;
         let mut channel_id=REMAP_TABLE[i].channel_id;
         if channel_id < 4{
